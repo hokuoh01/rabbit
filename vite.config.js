0a33e2b5f4c4b21fd0dc31e42378943877827375
Yu Bo <yu.ha@hrg.co.jp>
2024-06-02 16:55:18 +0900

    elementPlus自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
+//要引入 ElementPlus 组件，vite的配置文件一定要只有写，要不ElementPlus组件的样式不会呈现。
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    
+    AutoImport({
+      resolvers:[ElementPlusResolver()],
+    }),
+    Components({
+      resolvers:[ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
