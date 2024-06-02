1ab6e6df43e4676a76dbcc1cb6ef35cd91b3093d
Yu Bo <yu.ha@hrg.co.jp>
2024-06-02 19:46:11 +0900

    vue画面の設定、タイトル、体、お尻

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    我是首页
-    <RouterView/>
-    
-  </template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
