b80d982151b5f83f16bc66bd81f5c943e9844771
Yu Bo <yu.ha@hrg.co.jp>
2024-06-02 17:22:09 +0900

    elementのテーマ色定義

--- vite.config.js
+++ vite.config.js
@@ -14,14 +14,28 @@ export default defineConfig({
     AutoImport({
       resolvers:[ElementPlusResolver()],
     }),
+
+    //1 配置elementPlus采用sass样式配色系统
     Components({
-      resolvers:[ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({
+        importStyle:"sass"
+      })],
     }),
   ],
   resolve: {
     alias: {
       //实际的文件路径转换功能，  @  -》src
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
